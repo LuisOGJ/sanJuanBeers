816afd5081fc1e0166e207e1646cae88be3c75a2
Willson <orlando.98.gonzales@gmail.com>
2024-12-04 07:24:58 -0500

    TENGO MIEDO

---
+++ sanJuanBeers/Repository/BeerRepository.cs
@@ -0,0 +1,40 @@
+﻿using sanJuanBeers.Models;
+
+namespace sanJuanBeers.Repository
+{
+    public class BeerRepository : IRepository<Beer>
+    {
+        private StoreContext _Context;
+
+        public BeerRepository(StoreContext context) {
+            _Context = context;
+        }
+
+        public async Task Add(Beer beer) => await _Context.Beers.AddAsync(beer);
+
+        public void Delete(Beer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Beer>> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Beer> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Beer entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ sanJuanBeers/Validators/BeerValidators/BeerUpdateValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using sanJuanBeers.DTOs.BeerDTOs;
+
+namespace sanJuanBeers.Validators.BeerValidators
+{
+    public class BeerUpdateValidator : AbstractValidator<BeerUpdateDto>
+    {
+        public BeerUpdateValidator() {
+            RuleFor(x => x.BeerID).NotEmpty().WithMessage("ID obligatorio");
+            RuleFor(x => x.Name).NotEmpty().WithMessage("El nombre es obligatorio | Error personalizado en BeerInsertValidator");
+            RuleFor(x => x.Name).Length(2, 20).WithMessage("El nombre debe medir entre 2 o 20 caracteres");
+            RuleFor(x => x.BrandID).NotNull().WithMessage(x => "La marca es obligatoria");
+            RuleFor(x => x.BrandID).GreaterThan(0).WithMessage(x => "Error con valor enviado de marca");
+            RuleFor(x => x.Alcohol).GreaterThan(0).WithMessage(x => "El {PropertyName} debe ser mayor a 0");
+
+        }
+    }
+}
